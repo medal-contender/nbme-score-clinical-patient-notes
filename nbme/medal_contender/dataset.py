fa45d16a0069644b710183bbb41249fd5c03b4da
Young Min Paik <ympaik@hotmail.com>
2022-02-16 20:17:28 +0900

    add initial models

---
+++ nbme/medal_contender/dataset.py
@@ -0,0 +1,135 @@
+import ast
+import numpy as np
+import torch
+from torch.utils.data import Dataset, DataLoader
+from tqdm.auto import tqdm
+
+
+def prepare_input(CFG, text, feature_text):
+    inputs = CFG.tokenizer(
+        text, feature_text,
+        add_special_tokens=True,
+        max_length=CFG.max_len,
+        padding="max_length",
+        return_offsets_mapping=False
+    )
+    for k, v in inputs.items():
+        inputs[k] = torch.tensor(v, dtype=torch.long)
+    return inputs
+
+
+def create_label(CFG, text, annotation_length, location_list):
+    encoded = CFG.tokenizer(
+        text,
+        add_special_tokens=True,
+        max_length=CFG.max_len,
+        padding="max_length",
+        return_offsets_mapping=True
+    )
+    offset_mapping = encoded['offset_mapping']
+    ignore_idxes = np.where(np.array(encoded.sequence_ids()) != 0)[0]
+    label = np.zeros(len(offset_mapping))
+    label[ignore_idxes] = -1
+    if annotation_length != 0:
+        for location in location_list:
+            for loc in [s.split() for s in location.split(';')]:
+                start_idx = -1
+                end_idx = -1
+                start, end = int(loc[0]), int(loc[1])
+                for idx in range(len(offset_mapping)):
+                    if (start_idx == -1) & (start < offset_mapping[idx][0]):
+                        start_idx = idx - 1
+                    if (end_idx == -1) & (end <= offset_mapping[idx][1]):
+                        end_idx = idx + 1
+                if start_idx == -1:
+                    start_idx = end_idx
+                if (start_idx != -1) & (end_idx != -1):
+                    label[start_idx:end_idx] = 1
+    return torch.tensor(label, dtype=torch.float)
+
+
+def create_labels_for_scoring(df):
+    # example: ['0 1', '3 4'] -> ['0 1; 3 4']
+    df['location_for_create_labels'] = [ast.literal_eval(f'[]')] * len(df)
+    for i in range(len(df)):
+        lst = df.loc[i, 'location']
+        if lst:
+            new_lst = ';'.join(lst)
+            df.loc[i, 'location_for_create_labels'] = ast.literal_eval(f'[["{new_lst}"]]')
+    # create labels
+    truths = []
+    for location_list in df['location_for_create_labels'].values:
+        truth = []
+        if len(location_list) > 0:
+            location = location_list[0]
+            for loc in [s.split() for s in location.split(';')]:
+                start, end = int(loc[0]), int(loc[1])
+                truth.append([start, end])
+        truths.append(truth)
+    return truths
+
+
+class TrainDataset(Dataset):
+    def __init__(self, cfg, df):
+        self.cfg = cfg
+        self.feature_texts = df['feature_text'].values
+        self.pn_historys = df['pn_history'].values
+        self.annotation_lengths = df['annotation_length'].values
+        self.locations = df['location'].values
+
+    def __len__(self):
+        return len(self.feature_texts)
+
+    def __getitem__(self, item):
+        inputs = prepare_input(self.cfg,
+                               self.pn_historys[item],
+                               self.feature_texts[item])
+        label = create_label(self.cfg,
+                             self.pn_historys[item],
+                             self.annotation_lengths[item],
+                             self.locations[item])
+        return inputs, label
+
+
+def get_maxlen(dataframe, cfg):
+    len_li = []
+    for text_col in ['pn_history', 'feature_text']:
+        text_len = []
+        tk0 = tqdm(dataframe[text_col].fillna("").values,
+                   total=len(dataframe), desc=f'get max length for {text_col}')
+        for text in tk0:
+            length = len(
+                cfg.tokenizer(text, add_special_tokens=False)['input_ids'])
+            text_len.append(length)
+        len_li.append(text_len)
+    # cls & sep & sep
+    return max(len_li[0]) + max(len_li[1]) + 3
+
+
+def prepare_loaders(dataframe, CFG, fold=1):
+    train_folds = dataframe[dataframe['fold'] != fold].reset_index(drop=True)
+    valid_folds = dataframe[dataframe['fold'] == fold].reset_index(drop=True)
+    valid_texts = valid_folds['pn_history'].values
+    valid_labels = create_labels_for_scoring(valid_folds)
+
+    train_dataset = TrainDataset(CFG, train_folds)
+    valid_dataset = TrainDataset(CFG, valid_folds)
+
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=CFG.train_param.batch_size,
+        shuffle=True,
+        num_workers=CFG.train_param.num_workers,
+        pin_memory=True,
+        drop_last=True
+    )
+    valid_loader = DataLoader(
+        valid_dataset,
+        batch_size=CFG.train_param.batch_size,
+        shuffle=False,
+        num_workers=CFG.train_param.num_workers,
+        pin_memory=True,
+        drop_last=False
+    )
+
+    return train_loader, valid_loader, valid_texts, valid_labels, len(valid_folds), len(train_folds)
